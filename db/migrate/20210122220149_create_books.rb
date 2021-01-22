b98098e33334746a61b5b946ebb31b359f5c0717
elisa <elisaopalka@gmail.com>
2021-01-22 18:36:18 -0500

    updated books attributes

--- db/migrate/20210122220149_create_books.rb
+++ db/migrate/20210122220149_create_books.rb
@@ -2,10 +2,9 @@ class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :genre
-      t.integer :difficulty
+      t.string :class
+      t.string :difficulty
       t.integer :user_id
-      t.integer :spell_id
 
       t.timestamps
     end
