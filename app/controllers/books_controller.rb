41d29598476222d9cf55822492381b43da1b5717
elisa <elisaopalka@gmail.com>
2021-01-23 16:40:09 -0500

    books crud

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,30 @@
 class BooksController < ApplicationController
+    def index
+        @books = Book.all 
+    end
+
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+        3.times { @book.spells.build } #has_many
+    end
+
+    def create
+        @book = Book.new(book_params)
+        if @book.save 
+            redirect_to book_path(@book)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def book_params
+        params.require(:book).permit(:title, :class, :difficulty, spells_attributes: [:name,:description,:difficulty_level,:power_level])
+    end
+    
 end
