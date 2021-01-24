d73e052ee8c10d671026b481baef8d858e8694e8
elisa <elisaopalka@gmail.com>
2021-01-23 22:40:42 -0500

    added redirect if not logged in method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    include ApplicationHelper  #give us access to the helper methods in that module
+
+    private
+
+    def redirect_if_not_logged_in
+        redirect_to '/login' if !logged_in?
+    end
 end
