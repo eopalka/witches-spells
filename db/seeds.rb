e2a200737ce28e34112824b5f7bb7f60c095d4e8
elisa <elisaopalka@gmail.com>
2021-01-23 21:42:19 -0500

    added data to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,11 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #
+
+#    = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create([{ name: 'Harry Potter', email: 'thechosenone@gmail.com', password: 'Ginny'}])
+Book.create([{ title: 'Furious Scroll', genre: 'Dark', difficulty: 4, user_id: 1}])
+Spell.create([{ name: 'Mystic Rupture', description: 'Stuns opponents magical ability for 5 minutes', difficulty_level: 4, power_level: 6, book_id: 1}])
