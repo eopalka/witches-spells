b1945bc18ecdee977c9bedc17b929d462d588a1e
elisa <elisaopalka@gmail.com>
2021-01-23 23:24:16 -0500

    added new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
+
+  root to: 'sessions#welcome'
+
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  resources :users
+
   resources :wands
   resources :spells
   resources :books
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
