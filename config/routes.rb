0d6a634feda452e093b64d9b7ae1475ad105c662
elisa <elisaopalka@gmail.com>
2021-01-25 11:42:56 -0500

    added get and post route for logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
   
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+
+  get '/logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
 
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  resources :users
 
+  resources :users
   resources :wands
   resources :spells
   resources :books
