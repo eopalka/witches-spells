649b9d29042920f59eda7a6df184a3e000f15ea9
elisa <elisaopalka@gmail.com>
2021-01-26 00:28:24 -0500

    added comma

--- app/models/wand.rb
+++ app/models/wand.rb
@@ -1,6 +1,6 @@
 class Wand < ApplicationRecord
   belongs_to :user
 
-  validates :name, :wood, :core presence: true
+  validates :name, :wood, :core, presence: true
   validates :length, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 20}
 end
