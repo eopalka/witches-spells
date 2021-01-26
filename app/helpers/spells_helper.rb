24d83f4ca56a5bcbe4d07a669e4bf0329156f86a
elisa <elisaopalka@gmail.com>
2021-01-26 00:26:08 -0500

    added display header helper methods

--- app/helpers/spells_helper.rb
+++ app/helpers/spells_helper.rb
@@ -1,2 +1,26 @@
 module SpellsHelper
+
+    def display_index_header
+        if @book 
+        content_tag(:h1, "#{@book.title}  Spells")
+        else 
+        content_tag(:h1, "All Spells")
+        end 
+   end
+
+    def display_new_header
+        if @book 
+        content_tag(:h1, "Create a #{@book.title} Spell")
+        else 
+        content_tag(:h1, "Create a Spell")
+        end 
+   end
+
+    def book_form_fields(f)
+        if @book   # params[:brand_id]
+         f.hidden_field :book_id, value: @book.id 
+        else 
+         render partial: "book_fields", locals: { f: f } 
+        end 
+    end
 end
