88d456d6694000a271e35ef4328f12598f407306
elisa <elisaopalka@gmail.com>
2021-01-26 00:28:00 -0500

    added nested attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,6 @@ class Book < ApplicationRecord
 
     validates :title, :genre, presence: true
     validates :difficulty, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 5}
+
+    accepts_nested_attributes_for :spells, reject_if: proc { |attributes| attributes['name'].blank? || attributes['description'].blank? || attributes['difficulty_level'].blank? || attributes['power_level'].blank?  }
 end
