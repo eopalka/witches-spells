25e97dd6e66142e6440d08420bdc378221fef093
elisa <elisaopalka@gmail.com>
2021-01-28 19:31:26 -0500

    added bokk title unique

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,9 @@ class Book < ApplicationRecord
     has_many :spells
     belongs_to :user
 
-    validates :title, :genre, presence: true
+    
+    validates :title, presence: true, uniqueness: true
+    validates :genre, presence: true
     validates :difficulty, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 5}
 
     accepts_nested_attributes_for :spells, reject_if: proc { |attributes| attributes['name'].blank? || attributes['description'].blank? || attributes['difficulty_level'].blank? || attributes['power_level'].blank?  }
