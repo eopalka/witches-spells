7fd79c76c2a7a43ff46e3f04bd2cc70a14e174a6
elisa <elisaopalka@gmail.com>
2021-01-29 18:16:04 -0500

    alphbetical order method on books

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,8 @@ class Book < ApplicationRecord
     validates :difficulty, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 5}
 
     accepts_nested_attributes_for :spells, reject_if: proc { |attributes| attributes['name'].blank? || attributes['description'].blank? || attributes['difficulty_level'].blank? || attributes['power_level'].blank?  }
+
+    def self.alphabetize
+        self.order(title: :asc)
+    end
 end
