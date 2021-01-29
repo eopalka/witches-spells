1bf20d0a81cb8edeb7593d3efdbb4a65bd362a17
elisa <elisaopalka@gmail.com>
2021-01-28 19:31:43 -0500

    added email unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_many :spells, through: :books
     has_one :wand
     has_secure_password
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
