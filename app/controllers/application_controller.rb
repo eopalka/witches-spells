cdcb24bf28efea117231a490fe1505320903b673
elisa <elisaopalka@gmail.com>
2021-02-02 12:14:04 -0500

    added fallback method for inccorect urls

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::Base
         @current_user ||=User.find(session[:user_id]) if session[:user_id]
     end
     helper_method :current_user
+
+    def fallback
+        redirect_to user_path
+    end
    
 
     private
