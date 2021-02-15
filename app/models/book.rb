8f1a480150e35191e3ec9930ef5eaf9ddc0e0bc2
elisa <elisaopalka@gmail.com>
2021-02-15 11:44:19 -0500

    moved validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,6 @@ class Book < ApplicationRecord
     has_many :spells, dependent: :delete_all
     belongs_to :user
 
-    
     validates :title, presence: true, uniqueness: true
     validates :genre, presence: true
     validates :difficulty, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 5}
@@ -19,5 +18,4 @@ class Book < ApplicationRecord
     def self.most_recent_books
         Book.order(created_at: :desc).limit(4)
     end
-
 end
--- app/models/spell.rb
+++ app/models/spell.rb
@@ -5,13 +5,12 @@ class Spell < ApplicationRecord
     belongs_to :book
     has_many :users, through: :books
 
-
+    validates :name, :description, presence: true
+    validates :difficulty_level, :power_level, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 10}
+    
     def book_attributes=(hash_of_attributes)
         if !hash_of_attributes[:title].blank? && !hash_of_attributes[:genre].blank? && !hash_of_attributes[:difficulty].blank?
             self.book = Book.find_or_create_by(hash_of_attributes)
         end
     end
-
-    validates :name, :description, presence: true
-    validates :difficulty_level, :power_level, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 10}
 end
