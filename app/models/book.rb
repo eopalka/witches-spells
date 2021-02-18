c91ef8de6751b28796aae417e71e9a08ff2d1949
elisa <elisaopalka@gmail.com>
2021-02-18 01:29:51 -0500

    dry changes

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,7 +9,7 @@ class Book < ApplicationRecord
     validates :genre, presence: true
     validates :difficulty, presence: true, numericality: {greater_than: 0, less_than_or_equal_to: 5}
 
-    accepts_nested_attributes_for :spells, reject_if: proc { |attributes| attributes['name'].blank? || attributes['description'].blank? || attributes['difficulty_level'].blank? || attributes['power_level'].blank?  }
+    accepts_nested_attributes_for :spells, reject_if: :all_blank
 
     def self.alphabetize
         Book.order(title: :asc)
